197cc1317d10bb3c95c19247ca3d7f3b8c63a21e
dassell414 <10219530+dassell414@users.noreply.github.com>
2018-02-21 16:47:25 +0000

    Update alert_withretunfalsev2.js
    added some cookie script to set one after first check

--- alert_withretunfalsev2.js
+++ alert_withretunfalsev2.js
@@ -1,5 +1,17 @@
 <script>
-function() {
+
+// Check if Cookie exists
+function checkCookie() {
+
+    // Get the cookie called "visited"
+    var visited = getCookie("visited");
+
+    // If it exists, print the Cookie to the Console
+    if (visited == "true") {
+        console.log("cookie is here !!! do nowt ");
+    }
+    else {
+        function() {
  window.setTimeout(function() {
  alert('Please check availability - some of our gorgeous boots and shoes are custom made (( 01905 753254 | info@loveme-shoes-boots.co.uk ))');
 }, 5000);
@@ -7,4 +19,16 @@ function() {
 return false;
 	);
 }
+
+//  Set the Cookie
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+    }
+}
+
 </script>
