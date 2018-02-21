3542c7a86930954fa9ad57cf0c01ef812a81ef37
dassell414 <10219530+dassell414@users.noreply.github.com>
2018-02-21 16:23:37 +0000

    Add files via upload
    this script I only want to fire once in a session if user goes back to shopping site from cart

---
+++ alert_withretunfalsev2.js
@@ -0,0 +1,10 @@
+<script>
+function() {
+ window.setTimeout(function() {
+ alert('Please check availability - some of our gorgeous boots and shoes are custom made (( 01905 753254 | info@loveme-shoes-boots.co.uk ))');
+}, 5000);
+
+return false;
+	);
+}
+</script>
